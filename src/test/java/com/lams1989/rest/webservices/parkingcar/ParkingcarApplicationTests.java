5463c80e4f944ea720f827a79a8562a2c507d438
Luis Munoz <luisalejandromunozsierra@gmail.com>
2020-08-31 18:44:12 -0300

    first commit

---
+++ src/test/java/com/lams1989/rest/webservices/parkingcar/ParkingcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lams1989.rest.webservices.parkingcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
